d6fb98543fba716ca770d8f92c3a8f994144491f
AzeemNasir007 <azeem_nasir@outlook.com>
2022-08-29 11:28:21 -0700

    Update scripts.js
    adjusting js

--- js/scripts.js
+++ js/scripts.js
@@ -21,20 +21,18 @@ function newItem(){
     li.toggleClass("strike");
   });
     
-//3(i). Adding the delete button "X": 
-  li.addClass("delete");
+//3(i). Adding the delete button "X":
     
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
-  crossOutButton.on("click", deleteListItem);
-  
   
 //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  crossOutButton.on("click", deleteListItem);
   function deleteListItem(){
-    li.classList.add("delete")
+    li.addClass("delete")
   }
   
 // 4. Reordering the items: 
   $('#list').sortable();
-  }
+}
